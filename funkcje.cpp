1af41ba4a5c5f4a83a3305e26855ff5d96716587
DawidBarnas <dawidbarnas1998@gmail.com>
2021-11-12 13:50:35 +0100

    1.0

--- funkcje.cpp
+++ funkcje.cpp
@@ -4,9 +4,22 @@ using namespace std;
 
 int main()
 {
-	cout<<"Hello world";
+	float b,x,a=0;
 	
+	cout<<"Obliczanie miejsc zerowych funkcji liniowej gdzie a musi byæ rozne od 0. \nPodaj a : ";
+	cin>>a;
+	cout<<"Podaj b : ";
+	cin>>b;
+	if (a != 0)
+	{
+	x=((-b)/a);
 	
+	cout<<"\nMiejsce zerowe wynosi : "<<x;
+	}
+	else 
+	{
+		cout<<"Niewlasciwe dane, a musi byc rozne od 0";	
+	}
 	
 	return 0;
 }
