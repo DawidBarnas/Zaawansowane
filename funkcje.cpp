2eb56fe3bc71fb6d9f3954e5f1c2faac549f7758
kapizwa <kac.gorka@gmail.com>
2021-11-15 21:06:17 +0100

    2.5

--- funkcje.cpp
+++ funkcje.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
 
-	float b,x1,x2,a,c,delta,y;
+	float b,x1,x2,a,c,delta,y,p,q;
 	double pierw;
 	
     int wybor = 0;
@@ -18,6 +18,7 @@ int main()
     cout<<"**MENU**\n";
     cout<<"1.Funkcja liniowa\n";
     cout<<"2.Funkcja kwadratowa\n";
+    cout<<"3.Postac kanoniczna funkcji kwadratowej\n";
 
     cout<<"Wybierz:";
     wybor=getch();
@@ -112,7 +113,69 @@ int main()
 
                
             break;
+            
+        case '3':
+        	cout<<"Obliczanie funkcji kwadratowej w postaci kanonicznej. /nPodaj a : ";
+	cin>>a;
+	if(a!=0)
+	{
+		cout<<"Podaj b : ";
+		cin>>b;
+		cout<<"Podaj c : ";
+		cin>>c;
+		delta=(b*b)-(4*a*c);
+		cout<<"Delta wynosi: "<<delta;
+		pierw=sqrt(delta);
+		if(delta>0)
+		{
+		cout<<endl<<"Pierwiastek z delty wynosi: "<<pierw;
+		 p=-(b)/(2*a);
+		 q=-(delta)/(4*a);
+		 cout<<endl<<"p wynosi: "<<p;
+		 cout<<endl<<"q wynosi: "<<q;
+			
+	    }   
+	    else if(delta==0)
+	    {
+		 p=-(b)/(2*a);
+		 q=-(delta)/(4*a);
+		 cout<<endl<<"p wynosi: "<<p;
+		 cout<<endl<<"q wynosi: "<<q;
+		}
+		else
+		{
+		p=-(b)/(2*a);
+		 q=-(delta)/(4*a);
+		 cout<<endl<<"p wynosi: "<<p;
+		 cout<<endl<<"q wynosi: "<<q;;
+		}
+	}
+	else
+	{
+	cout<<"Podaj b : ";
+	cin>>b;
+	cout<<"Podaj c : ";
+	cin>>c;
+	if (b != 0)
+	if (b == 0 && c == 0)
+	{
+		cout<<"Funkcja ma nieskonczenie wiele miejsc zerowych.";
+	}else if (b == 0 && c != 0)
+	{
+		cout<<"Funkcja nie ma miejsc zerowych.";
+	}else
+	{
+	y=((-c)/b);
 
+	cout<<"\nMiejsce zerowe wynosi : "<<y;
+	}
+	else 
+	{
+		cout<<"Niewlasciwe dane, a musi byc rozne od 0";	
+	}
+
+    }
+	
     }
     
 
