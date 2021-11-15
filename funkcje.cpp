bb4672d34e677928b1e2a52357976cb053e17fee
DawidBarnas <dawidbarnas1998@gmail.com>
2021-11-15 19:13:51 +0100

    2.0

--- funkcje.cpp
+++ funkcje.cpp
@@ -8,34 +8,37 @@ int main()
 	float b,x1,x2,a,c,delta;
 	double pierw;
 	
-	cout<<"Obliczanie miejsc zerowych funkcji liniowej. \nPodaj a : ";
+	cout<<"Obliczanie miejsc zerowych funkcji kwadratowej gdzie a musi byc rozne od 0. \nPodaj a : ";
 	cin>>a;
-	cout<<"Podaj b : ";
-	cin>>b;
-	cout<<"Podaj c : ";
-	cin>>c;
-	delta=(b*b)-(4*a*c);
-	cout<<"Delta wynosi: "<<delta;
-	pierw=sqrt(delta);
-	if(delta>0)
+	if(a!=0)
 	{
-		cout<<endl<<"Pierwiastek z delty wynosi: "<<pierw;
-	x1=(-b+pierw)/(2*a);
-	x2=(-b-pierw)/(2*a);
-	cout<<endl<<"Miejsca zerowe: "<<endl;
-	cout<<x1;
-	cout<<endl<<x2;
-    }   
-    else if(delta==0)
-    {
-    x1=-b/(a*a);
-    cout<<endl<<"Miejsce zerowe: "<<endl;
-    cout<<x1;
-	}
-	else
-	{
-		cout<<endl<<"Brak miejsc zerowych ";
-	}
+		cout<<"Podaj b : ";
+		cin>>b;
+		cout<<"Podaj c : ";
+		cin>>c;
+		delta=(b*b)-(4*a*c);
+		cout<<"Delta wynosi: "<<delta;
+		pierw=sqrt(delta);
+		if(delta>0)
+		{
+			cout<<endl<<"Pierwiastek z delty wynosi: "<<pierw;
+			x1=(-b+pierw)/(2*a);
+			x2=(-b-pierw)/(2*a);
+			cout<<endl<<"Miejsca zerowe: "<<endl;
+			cout<<x1;
+			cout<<endl<<x2;
+	    }   
+	    else if(delta==0)
+	    {
+		    x1=-b/(a*a);
+		    cout<<endl<<"Miejsce zerowe: "<<endl;
+		    cout<<x1;
+		}
+		else
+		{
+			cout<<endl<<"Brak miejsc zerowych ";
+		}
+	}else cout<<"Bledne dane, a musi byc rozne od 0.";
 
 	
 	return 0;
