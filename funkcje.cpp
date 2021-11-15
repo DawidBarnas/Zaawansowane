9f5b3e44b0b15a689741ed772e6c3baab1a0deac
kapizwa <kac.gorka@gmail.com>
2021-11-15 19:26:13 +0100

    2.2

--- funkcje.cpp
+++ funkcje.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	float b,x1,x2,a,c,delta;
+	float b,x1,x2,a,c,delta,y;
 	double pierw;
 	
 	cout<<"Obliczanie miejsc zerowych funkcji kwadratowej gdzie a musi byc rozne od 0. \nPodaj a : ";
@@ -38,7 +38,31 @@ int main()
 		{
 			cout<<endl<<"Brak miejsc zerowych ";
 		}
-	}else cout<<"Bledne dane, a musi byc rozne od 0.";
+	}
+	else
+	{
+	cout<<"Podaj b : ";
+	cin>>b;
+	cout<<"Podaj c : ";
+	cin>>c;
+	if (b != 0)
+	if (b == 0 && c == 0)
+	{
+		cout<<"Funkcja ma nieskonczenie wiele miejsc zerowych.";
+	}else if (b == 0 && c != 0)
+	{
+		cout<<"Funkcja nie ma miejsc zerowych.";
+	}else
+	{
+	y=((-c)/b);
+
+	cout<<"\nMiejsce zerowe wynosi : "<<y;
+	}
+	else 
+	{
+		cout<<"Niewlasciwe dane, a musi byc rozne od 0";	
+	}
+    }
 
 	
 	return 0;
